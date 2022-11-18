eb9b433979c71bcc48c101e20638af8b060f33db
stevenjiao <syjiao@ualberta.ca>
2022-11-18 11:07:48 -0700

    testing out npm web-bt package

--- main.js
+++ main.js
@@ -4,39 +4,90 @@
 "use strict";
 
 const { strict, strictEqual } = require('assert');
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const bluetooth = require("webbluetooth").bluetooth;
 const path = require('path');
+let win = null;
+let server = null;
+let characteristic = null;
+let service = null;
+let device = null;
 
+const SERVICE_UUID = '4fafc201-1fb5-459e-8fcc-c5c9c331914b';
+const CHARACTERISTIC_UUID = 'beb5483e-36e1-4688-b7f5-ea07361b26a8';
 
-function createWindow() {
-   const win = new BrowserWindow({
+async function createWindow() {
+   win = new BrowserWindow({
       width: 800,
       height: 600,
+      webPreferences: {
+         nodeIntegration: true,
+         contextIsolation: false,
+      }
    });
+
+   win.webContents.openDevTools();
 
    // Device scans start with calling Bluetooth.requestDevice(...) on the renderer side.  
    // When devices are found, this event is triggered.  We can use this event to present 
    // a device picker to the user, or we can automaically pick something.
-   win.webContents.on('select-bluetooth-device', (event, devices, callback) => {
-      event.preventDefault()
+   // win.webContents.on('select-bluetooth-device', (event, devices, callback) => {
+   //    event.preventDefault()
 
-      // just pick the first device discovered (requestDevice() handles filtering)
-      if (devices && devices.length > 0) {
-         callback(devices[0].deviceId)
-      }
-   });
+   //    // just pick the first device discovered (requestDevice() handles filtering)
+   //    if (devices && devices.length > 0) {
+   //       callback(devices[0].deviceId)
+   //    }
+   // });
+
+   // device = await bluetooth.requestDevice({
+   //    filters:[{ services: [SERVICE_UUID] }]
+   // });
+
+   // server = await device.gatt.connect();
+
+   // service = await server.getPrimaryService(SERVICE_UUID);
+
+   // characteristic = await service.getCharacteristic(CHARACTERISTIC_UUID);
 
    win.loadFile('index.html');
 }
 
-app.whenReady().then(() => {
-   createWindow();
+app.whenReady().then(async () => {
+   await createWindow();
 
-   app.on('activate', function () {
+   app.on('activate', async () => {
       if (BrowserWindow.getAllWindows().length === 0) {
-         createWindow();
+         await createWindow();
       }
    });
+});
+
+ipcMain.on('refresh', (_event) => {
+   win.loadFile('index.html');
+});
+
+ipcMain.on('connect-device', async (_event) => {
+   device = await bluetooth.requestDevice({
+      filters:[{ services: [SERVICE_UUID] }]
+   });
+
+   server = await device.gatt.connect();
+
+   service = await server.getPrimaryService(SERVICE_UUID);
+
+   characteristic = await service.getCharacteristic(CHARACTERISTIC_UUID);
+});
+
+ipcMain.on('write-to-device', async (_event, data) => {
+   try { 
+      console.log(data);
+      console.log(Buffer.from(data, 'utf8'));
+      let resp = await characteristic.writeValueWithResponse(Buffer.from(data, 'utf8'));
+      console.log(`the response: ${resp}`);
+      // await characteristic.writeValue(Buffer.from(['3C', '31', ]));
+   }
+   catch (err) { console.error(err); }
 });
 
 app.on('window-all-closed', function () {
--- renderer.js
+++ renderer.js
@@ -1,3 +1,5 @@
+const { ipcRenderer } = require('electron');
+
 // For this example, a BLE device needs to have the following service UUID with
 // a readable/writeable characteristic.
 const SERVICE_UUID = '4fafc201-1fb5-459e-8fcc-c5c9c331914b';
@@ -6,12 +8,23 @@ const CHARACTERISTIC_UUID = 'beb5483e-36e1-4688-b7f5-ea07361b26a8';
 
 const statusLabel = document.getElementById('statusLabel');
 const connectButton = document.getElementById('connectButton');
+const refreshButton = document.getElementById('refreshButton');
+const enterButton = document.getElementById('enterButton');
+let writeInput = document.getElementById('writeInput');
 
-connectButton.addEventListener('click', testIt);
+refreshButton.addEventListener('click', refresh);
+connectButton.addEventListener('click', connectToDevice);
+enterButton.addEventListener('click', writeToDeviceMain);
+// enterButton.addEventListener('click', writeToDeviceRenderer);
+// connectButton.addEventListener('click', testIt);
+
+let service = null;
+// let characteristic = null;
 
 // Display what's happening to user
 function displayStatus(status) {
    statusLabel.innerHTML = status;
+   console.log(status);
 }
 
 // Find device by service, connect, read, write
@@ -31,29 +44,41 @@ async function testIt() {
       const server = await device.gatt.connect();
       displayStatus('Connected to GATT server');
 
-      const service = await server.getPrimaryService(SERVICE_UUID);
+      service = await server.getPrimaryService(SERVICE_UUID);
       displayStatus('Connected to service ' + SERVICE_UUID);
-
-      const characteristic = await service.getCharacteristic(CHARACTERISTIC_UUID);
-      let readData = await characteristic.readValue();
-
-      // the characteristic may not have a value (empty buffer), we will init to zero if so
-      let value = 0;
-      if (readData.byteLength > 0) {
-         value = readData.getUint8(0);
-         displayStatus('Characteristic ' + CHARACTERISTIC_UUID + ' is ' + value.toString(16));
-         value++;  // increment for writing later
-      }
-
-      // write new value to the characteristic (assumes "write with response")
-      let writeBuffer = new ArrayBuffer(1);
-      let writeData = new DataView(writeBuffer);
-      writeData.setUint8(0, value);
-      characteristic.writeValueWithResponse(writeData);
-      displayStatus('Characteristic ' + CHARACTERISTIC_UUID + ' set to ' + value.toString(16));
-
    } catch (error) {
       displayStatus("Error: " + error);
    }
 }
 
+async function connectToDevice() {
+   ipcRenderer.send('connect-device');
+}
+
+async function refresh() {
+   ipcRenderer.send('refresh');
+}
+
+async function writeToDeviceMain() {
+   // try {
+   //    const characteristic = await service.getCharacteristic(CHARACTERISTIC_UUID);
+   //    characteristic.writeValueWithResponse(Buffer.from(writeInput.value, "utf-8"));
+   //    displayStatus('Characteristic ' + CHARACTERISTIC_UUID + ' set to ' + writeInput.value);
+   // }
+   // catch(err) {
+   //    displayStatus("Error: " + err);
+   // }
+
+   ipcRenderer.send('write-to-device', writeInput.value);
+}
+
+async function writeToDeviceRenderer() {
+   try {
+      const characteristic = await service.getCharacteristic(CHARACTERISTIC_UUID);
+      characteristic.writeValueWithResponse(Buffer.from(writeInput.value, "utf-8"));
+      displayStatus('Characteristic ' + CHARACTERISTIC_UUID + ' set to ' + writeInput.value);
+   }
+   catch(err) {
+      displayStatus("Error: " + err);
+   }
+}
